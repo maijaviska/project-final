4f49505d25f7f454c701de97373254a3d8c6e4ed
maijaviska <48285008+maijaviska@users.noreply.github.com>
2022-03-11 15:22:11 +0200

    small logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom"
+
 function Footer() {
     return (
         <div>
+
             <footer className="p-5 bg-dark text-white text-cener position-relative">
                 <div className="container">
-                    <p className="lead">Copyright &copy; 2022 Purrfect bean</p>
+                    <div className="row align-items-center justify-content-end">
+                        <div className="col-4">
+                            <p className="small">Copyright &copy; 2022 Purrfect bean</p>
+                        </div>
+                        <div className="col-4">
+                            <Link className="nav-link" to={'/'}>
+                                <img src="/Images/purrfect_bean5.png" className="mx-auto d-block" style={{ width: '5rem' }} alt="purr_bean" />
+                            </Link>
+                        </div>
+                        <div className="col-4">
+                            <p className="small">Copyright &copy; 2022 Purrfect bean</p>
+                        </div>
+                    </div>
                 </div>
             </footer>
         </div>
