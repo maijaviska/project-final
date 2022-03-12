5aff654aa22ca0038ff209016d394490a2b2be3a
maijaviska <48285008+maijaviska@users.noreply.github.com>
2022-03-12 10:21:46 +0200

    registration edit

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -26,13 +26,13 @@ function Register() {
                                         </div>
 
                                         <div className="form-outline mb-4">
-                                            <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                                            <label className="form-label" for="form3Example1cg">First Name</label>
+                                            <input type="text" id="form3Example2cg" className="form-control form-control-lg" />
+                                            <label className="form-label" for="form3Example2cg">First Name</label>
                                         </div>
 
                                         <div className="form-outline mb-4">
-                                            <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                                            <label className="form-label" for="form3Example1cg">Last Name</label>
+                                            <input type="text" id="form3Example5cg" className="form-control form-control-lg" />
+                                            <label className="form-label" for="form3Example5cg">Last Name</label>
                                         </div>
 
                                         <div className="form-outline mb-4">
@@ -63,7 +63,7 @@ function Register() {
                                         </div>
 
                                         <div className="d-flex justify-content-center">
-                                            <button type="button" className="btn btn-warning btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                                            <button type="submit" className="btn btn-warning btn-block btn-lg gradient-custom-4 text-body">Register</button>
                                         </div>
 
                                         <p className="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!" className="fw-bold text-body"><u>Login here</u></a></p>
