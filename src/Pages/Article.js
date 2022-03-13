4c149d31fd96d09a2fb45044f6e2989d26df82d7
maijaviska <48285008+maijaviska@users.noreply.github.com>
2022-03-13 12:06:36 +0200

    fancybox

--- src/Pages/Article.js
+++ src/Pages/Article.js
@@ -1,18 +1,81 @@
 import { Link, useParams } from "react-router-dom"
 import getArticleByIndex from "../API/getArticleByIndex"
 
-function Article(){
-    const {index} = useParams()
+function Article() {
+    const { index } = useParams()
     const article = getArticleByIndex(index)
-    
-    return(
+    const imageStyle = { maxWidth: '300px' };
+    const imageStyle2 = { maxWidth: '150px' };
+
+    return (
         <div>
-        <h5>
-            <Link to="/articles">Back</Link>
-        </h5>
-        <h2>{article.title}</h2>
-        <p>{article.describtione}</p>
-    </div>
+
+            <h5>
+                <Link to="/articles">Back</Link>
+            </h5>
+            <div className="row">
+                <div className="col-12">
+                    <h2>{article.title}</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4">
+                    <div className="row">
+                        <div className="col">
+                            <a data-fancybox href={article.image1}>
+                                <img className="fancy-box-image" src={article.image1} style={imageStyle} alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-8">
+                    <p>{article.describtione}</p>
+                    <div className="row d-flex mt-3 ">
+
+                        <div className="col-6 text-center">
+                            <p className="one-product-price">{article.price} EUR</p>
+                        </div>
+
+                        <div className="col-6 text-center">
+                            <button className="btn btn-success address-btn" id="address-button" onclick="addAddress()">Add to cart</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="row mt-3 mb-3">
+                    <div className="col-6 first-left">
+                        <div className="row d-flex">
+                            
+                            <div className="col-4">
+                                <a data-fancybox href={article.image2}>
+                                    <img className="fancy-box-image" src={article.image2} style={imageStyle2} alt="" />
+                                </a>
+                            </div>
+                            <div className="col-4">
+                                <a data-fancybox href={article.image3}>
+                                    <img className="fancy-box-image" src={article.image3} style={imageStyle2} alt="" />
+                                </a>
+                            </div>
+                            <div className="col-4">
+                                <a data-fancybox href={article.image4}>
+                                    <img className="fancy-box-image" src={article.image4} style={imageStyle2} alt="" />
+                                </a>
+                            </div>
+                        </div>
+
+
+
+
+
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
     )
 }
 export default Article
--- src/Pages/Chat.js
+++ src/Pages/Chat.js
@@ -11,18 +11,34 @@ function Chat() {
                                 style={{ borderTop: "4px solid #ffa900;" }}>
                                 <h5 className="mb-0">Chat messages</h5>
                                 <div className="d-flex flex-row align-items-center">
-                                    <span className="badge bg-warning me-3">20</span>
-                                   
+                                    {/* <span className="badge bg-warning me-3">20</span> */}
+
                                 </div>
                             </div>
-                            <div className="card-body" data-mdb-perfect-scrollbar="true" style={{ position: "relative", height: "400px" }}>
+                            <div className="card-body" style={{ position: "relative", height: "400px" }}>
+                                <div className="d-flex flex-row justify-content-end mb-4 pt-1">
+                                        <div class="d-flex justify-content-between">
+                                            <p class="small mb-1">Johny Bullock</p>
+                                        </div>
+                                        <p className="small p-2 me-3 mb-3 text-white rounded-3 bg-warning">Thank you for your believe in our
+                                            supports</p>
 
-                            
+                                </div>
+                                <div className="d-flex flex-row justify-content-start">
+                                    <div className="d-flex justify-content-between">
+                                        <p className="small mb-1">Timona Siera</p>
+                                    </div>
+                                    <div>
+                                        <p className="small p-2 ms-3 mb-3 rounded-3" style={{ backgroundColor: "#f5f6f7" }}>For what reason would it
+                                            be advisable for me to think about business content?</p>
+                                    </div>
+                                </div>
+
                             </div>
                             <div className="card-footer text-muted justify-content-start align-items-center p-3">
                                 <ul className="mb-3">
                                     <div className="input p-3">
-                                    <input type="text" className="form-control" placeholder="Username"/>
+                                        <input type="text" className="form-control" placeholder="Username" />
                                     </div>
                                     <div className="form-outline form-white">
                                         <textarea className="form-control" id="textAreaExample3" rows="4"></textarea>
